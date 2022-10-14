1d0a2f6770ae5d7c33b18d0bdd0a7ab0c78a1753
LemuelKL <lemuellee.kl@gmail.com>
2022-10-14 16:51:31 +0800

    basic

--- main.c
+++ main.c
@@ -3,44 +3,67 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+void sigint_handler(int signum)
+{
+    return;
+}
 
 void show_prompt()
 {
-    printf("$$ 3230shell ##  ");
+    static int first_time = 1;
+    if (first_time)
+    {
+        // printf("%s", " \e[1;1H\e[2J");
+        printf("$$ 3230shell ##  ");
+        first_time = 0;
+    }
+    else
+    {
+        printf("\n$$ 3230shell ##  ");
+    }
 }
 
-void read_command(char *command[])
+void read_command(char *command[30])
 {
-    char line[1024];
     const char delim[2] = " ";
-    char *token;
-    scanf("%99[^\n]", line);
-    token = strtok(line, delim);
+    char line[1024];
+    scanf(" %99[^\n]", line);
 
     int i = 0;
-    while (token != NULL)
+    command[i] = strtok(line, delim);
+    while (command[i] != NULL)
     {
-        token = strtok(NULL, delim);
-        strcpy(command[i], token);
-        i++;
+        command[++i] = strtok(NULL, delim);
     }
 }
 
 int main(int argc, char **argv)
 {
+    signal(SIGINT, sigint_handler);
+
     char *command[30];
     while (1)
     {
         show_prompt();
         read_command(command);
-        printf("command: %s", command[0]);
-        if (fork() == 0) // child
+        int pid = fork();
+        if (pid == -1)
         {
-            execlp("ls", "ls", "-l", NULL);
+            printf("Error: fork failed");
+        }
+        else if (pid == 0) // child
+        {
+            int exec_status = execvp(command[0], command);
+            if (errno == ENOENT)
+                printf("3230shell: '%s': No such file or directory", command[0]);
+            if (errno == EACCES)
+                printf("3230shell: '%s': Permission denied", command[0]);
         }
         else // parent
         {
-            wait(NULL); // wait for child to finish
+            wait(NULL);
         }
     }
     return 0;
