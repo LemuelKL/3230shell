9722f8e8d4783655810a8426b88a1f7b0b76112d
LemuelKL <lemuellee.kl@gmail.com>
2022-10-15 18:10:43 +0800

    progress

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
+// Lee Kwok Lam
+// 3035782231
+// Developed on Windows 11 with WSL2 Ubuntu 20.04
+
+// The shell is the root parent.
+// Each prompt is a new child of the shell
+// Each command is a new child of the prompt, i.e. the grandchild of the shell
+
+#include <sys/resource.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <stdbool.h>
 #include <unistd.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
 
 #define MAX_LINE_LEN 1024
 #define MAX_WORD_NUM 30
 
 static int main_pid;
-static int prompt_pid;
 
 void sigint_handler(int signum)
 {
-    if (getpid() == main_pid)
+    int pid = getpid();
+    int ppid = getppid();
+    if (pid == main_pid) // main shell process uninterruptable
         return;
-    printf("\n");
-    exit(0);
-}
-
-void show_prompt()
-{
-    printf("$$ 3230shell ##  ");
+    else if (ppid == main_pid) // prompt process
+    {
+        printf("\n");
+        exit(0);
+    }
+    else
+    {
+        printf("!!!!!");
+    }
 }
 
 int read_command(char *cmd[30])
@@ -58,10 +71,20 @@ int interactive_prompt()
 {
     char *cmd[MAX_WORD_NUM + 1];
 
-    show_prompt();
+    printf("$$ 3230shell ##  ");
     int cmd_cnt = read_command(cmd);
     cmd[cmd_cnt] = NULL;
 
+    for (int i = 0; i < cmd_cnt; i++)
+    {
+        if (strcmp("&", cmd[i]) == 0 && (i != cmd_cnt - 1 || i == 0))
+        {
+            printf("3230shell: '&' should not appear in the middle of the command line\n");
+            return 1;
+        }
+    }
+
+    bool timeX = false;
     if (strcmp(cmd[0], "") == 0)
     {
         return 0;
@@ -79,15 +102,48 @@ int interactive_prompt()
     {
         if (cmd_cnt == 1)
         {
-            printf("3230shell: 'timeX' cannot be a standalone command");
+            printf("3230shell: 'timeX' cannot be a standalone command\n");
             return 1;
         }
+        if (strcmp(cmd[cmd_cnt - 1], "&") == 0)
+        {
+            printf("3230shell: 'timeX' cannot be run in background mode\n");
+            return 1;
+        }
+        timeX = true;
     }
-    int exec_status = execvp(cmd[0], cmd);
-    if (errno == ENOENT)
-        printf("3230shell: '%s': No such file or directory\n", cmd[0]);
-    if (errno == EACCES)
-        printf("3230shell: '%s': Permission denied\n", cmd[0]);
+    bool handle_signals = (signal(SIGINT, SIG_IGN) != SIG_IGN); // check if previous handler is SIG_IGN
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        if (handle_signals)
+            signal(SIGINT, SIG_DFL); // purge SIGINT handler
+        int exec_status;
+        if (timeX)
+            exec_status = execvp(cmd[1], cmd + 1);
+        else
+            exec_status = execvp(cmd[0], cmd);
+        if (exec_status == -1)
+            printf("3230shell: '%s': %s\n", cmd[0], strerror(errno));
+    }
+    else if (pid != -1)
+    {
+        int status;
+        int corpse = waitpid(pid, &status, 0);
+        if (WIFSIGNALED(status))
+            printf("%s\n", strsignal(WTERMSIG(status)));
+
+        if (timeX)
+        {
+            struct rusage rusage;
+            int ret = wait4(pid, &status, 0, &rusage);
+            printf("(PID)%d  (CMD)%s    (user)%.3f s  (sys)%.3f s\n",
+                   pid,
+                   cmd[1],
+                   rusage.ru_utime.tv_sec + rusage.ru_utime.tv_usec / 1000000.0,
+                   rusage.ru_stime.tv_sec + rusage.ru_stime.tv_usec / 1000000.0);
+        }
+    }
 
     return 0;
 }
@@ -117,9 +173,7 @@ int main(int argc, char **argv)
         }
         else // parent
         {
-            prompt_pid = pid;
             main_pid = getpid();
-            // kill(pid, SIGCONT);
             kill(pid, SIGUSR1);
 
             int status;
@@ -128,10 +182,11 @@ int main(int argc, char **argv)
             switch (exit_status)
             {
             case 0:
+                // prompt exists normally, let's spawn a new one
                 break;
             case 1:
-                break;
-            case 15:
+                // prompt (not command) exits with general error,
+                // probably due to invalid command, let's spawn a new one
                 break;
             case 100:
                 printf("3230shell: exiting\n");
@@ -139,6 +194,7 @@ int main(int argc, char **argv)
                 break;
 
             default:
+                printf("3230shell: unknown error code %d\n", exit_status);
                 break;
             }
         }
