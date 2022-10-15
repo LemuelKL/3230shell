2eeb54878740d284c8869a4514c74ab50cc47373
LemuelKL <lemuellee.kl@gmail.com>
2022-10-16 00:01:08 +0800

    flat

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 // 3035782231
 // Developed on Windows 11 with WSL2 Ubuntu 20.04
 
+///// OVERVIEW /////
 // The shell is the root parent.
 // Each prompt is a new child of the shell
-// Each command is a new child of the prompt, i.e. the grandchild of the shell
+// Each command run as the prompt process
 
 #include <sys/resource.h>
 #include <sys/wait.h>
@@ -17,25 +18,6 @@
 
 #define MAX_LINE_LEN 1024
 #define MAX_WORD_NUM 30
-
-static int main_pid;
-
-void sigint_handler(int signum)
-{
-    int pid = getpid();
-    int ppid = getppid();
-    if (pid == main_pid) // main shell process uninterruptable
-        return;
-    else if (ppid == main_pid) // prompt process
-    {
-        printf("\n");
-        exit(0);
-    }
-    else
-    {
-        printf("!!!!!");
-    }
-}
 
 int read_command(char *cmd[30])
 {
@@ -67,136 +49,181 @@ int read_command(char *cmd[30])
     }
 }
 
-int interactive_prompt()
+static int shell_pid = -1;
+void sigint_handler(int signum)
 {
-    char *cmd[MAX_WORD_NUM + 1];
-
-    printf("$$ 3230shell ##  ");
-    int cmd_cnt = read_command(cmd);
-    cmd[cmd_cnt] = NULL;
-
-    for (int i = 0; i < cmd_cnt; i++)
+    int pid = getpid();
+    int ppid = getppid();
+    if (pid == shell_pid)
     {
-        if (strcmp("&", cmd[i]) == 0 && (i != cmd_cnt - 1 || i == 0))
-        {
-            printf("3230shell: '&' should not appear in the middle of the command line\n");
-            return 1;
-        }
+        return;
     }
-
-    bool timeX = false;
-    if (strcmp(cmd[0], "") == 0)
+    else if (ppid == shell_pid)
     {
-        return 0;
+        printf("\n");
+        exit(0);
     }
-    if (strcmp(cmd[0], "exit") == 0)
-    {
-        if (cmd_cnt != 1)
-        {
-            printf("3230shell: 'exit' has too many arguments\n");
-            return 1;
-        }
-        return 100;
-    }
-    if (strcmp(cmd[0], "timeX") == 0)
-    {
-        if (cmd_cnt == 1)
-        {
-            printf("3230shell: 'timeX' cannot be a standalone command\n");
-            return 1;
-        }
-        if (strcmp(cmd[cmd_cnt - 1], "&") == 0)
-        {
-            printf("3230shell: 'timeX' cannot be run in background mode\n");
-            return 1;
-        }
-        timeX = true;
-    }
-    bool handle_signals = (signal(SIGINT, SIG_IGN) != SIG_IGN); // check if previous handler is SIG_IGN
-    pid_t pid = fork();
-    if (pid == 0)
-    {
-        if (handle_signals)
-            signal(SIGINT, SIG_DFL); // purge SIGINT handler
-        int exec_status;
-        if (timeX)
-            exec_status = execvp(cmd[1], cmd + 1);
-        else
-            exec_status = execvp(cmd[0], cmd);
-        if (exec_status == -1)
-            printf("3230shell: '%s': %s\n", cmd[0], strerror(errno));
-    }
-    else if (pid != -1)
-    {
-        int status;
-        int corpse = waitpid(pid, &status, 0);
-        if (WIFSIGNALED(status))
-            printf("%s\n", strsignal(WTERMSIG(status)));
-
-        if (timeX)
-        {
-            struct rusage rusage;
-            int ret = wait4(pid, &status, 0, &rusage);
-            printf("(PID)%d  (CMD)%s    (user)%.3f s  (sys)%.3f s\n",
-                   pid,
-                   cmd[1],
-                   rusage.ru_utime.tv_sec + rusage.ru_utime.tv_usec / 1000000.0,
-                   rusage.ru_stime.tv_sec + rusage.ru_stime.tv_usec / 1000000.0);
-        }
-    }
-
-    return 0;
 }
 
-void start_child(int signum)
+void sigchld_handler(int sig)
 {
-    return;
+    int status;
+    pid_t pid = wait(&status);
+}
+
+volatile sig_atomic_t execute = 0;
+void sigusr1_handler(int sig)
+{
+    // printf("[SIGUSR1] %d\n", getpid());
+    if (sig == SIGUSR1)
+    {
+        shell_pid = getppid();
+        execute = 1;
+    }
 }
 
 int main(int argc, char **argv)
 {
     signal(SIGINT, sigint_handler);
-    int run = 1;
-    while (run)
+    signal(SIGCHLD, sigchld_handler);
+    signal(SIGUSR1, sigusr1_handler);
+    char *cmd[MAX_WORD_NUM + 1];
+    pid_t pid;
+    while (1)
     {
-        pid_t pid = fork();
-        if (pid == -1)
+        pid = fork();
+        if (pid == 0)
         {
-            printf("Error: fork failed");
-        }
-        else if (pid == 0) // child
-        {
-            signal(SIGUSR1, start_child);
-            pause();
-            int x = interactive_prompt();
-            exit(x);
-        }
-        else // parent
-        {
-            main_pid = getpid();
-            kill(pid, SIGUSR1);
-
-            int status;
-            int child_pid = waitpid(pid, &status, WUNTRACED);
-            int exit_status = WEXITSTATUS(status);
-            switch (exit_status)
+            while (!execute)
             {
-            case 0:
-                // prompt exists normally, let's spawn a new one
-                break;
-            case 1:
-                // prompt (not command) exits with general error,
-                // probably due to invalid command, let's spawn a new one
-                break;
-            case 100:
-                printf("3230shell: exiting\n");
-                run = 0;
-                break;
-
-            default:
-                printf("3230shell: unknown error code %d\n", exit_status);
-                break;
+                pause();
             }
+            printf("$$ 3230shell ## ");
+            int cmd_cnt = read_command(cmd);
+            cmd[cmd_cnt] = NULL;
+
+            if (strcmp(cmd[0], "") == 0)
+                exit(0);
+            if (strcmp(cmd[0], "exit") == 0)
+            {
+                if (cmd_cnt != 1)
+                {
+                    printf("3230shell: 'exit' has too many arguments\n");
+                    exit(0);
+                }
+                exit(100);
+            }
+            if (strcmp(cmd[0], "timeX") == 0)
+            {
+                if (cmd_cnt < 2)
+                {
+                    printf("3230shell: 'timeX' cannot be a stnadalone command\n");
+                    exit(0);
+                }
+                if (strcmp(cmd[cmd_cnt - 1], "&") == 0)
+                {
+                    printf("3230shell: 'timeX' cannot be run in background mode\n");
+                    exit(0);
+                }
+
+                int timeX_pid = fork();
+                if (timeX_pid == 0)
+                {
+                    if (execvp(cmd[1], cmd + 1) == -1)
+                        printf("3230shell: '%s': %s\n", cmd[1], strerror(errno));
+                    exit(0);
+                }
+                else
+                {
+                    int status;
+                    struct rusage rusage;
+                    wait4(timeX_pid, &status, 0, &rusage);
+                    printf("(PID)%d  (CMD)%s    (user)%.3f s  (sys)%.3f s\n",
+                           timeX_pid,
+                           cmd[1],
+                           rusage.ru_utime.tv_sec + rusage.ru_utime.tv_usec / 1000000.0,
+                           rusage.ru_stime.tv_sec + rusage.ru_stime.tv_usec / 1000000.0);
+                    exit(0);
+                }
+            }
+            // PIPE
+            bool is_pipe = false;
+            for (int i = 0; i < cmd_cnt; i++)
+            {
+                if (strcmp(cmd[i], "|") == 0)
+                {
+                    is_pipe = true;
+                    break;
+                }
+            }
+            if (is_pipe)
+            {
+                for (int i = 0; i < cmd_cnt; i++)
+                {
+                    if (strcmp(cmd[i], "|") == 0 && (i == 0 || i == cmd_cnt - 1))
+                    {
+                        printf("3230shell: | cannot be the head or tail of the command\n");
+                        exit(0);
+                    }
+                    if (strcmp(cmd[i], "|") == 0 && strcmp(cmd[i + 1], "|") == 0)
+                    {
+                        printf("3230shell: should not have two consectuive | without in-between command\n");
+                        exit(0);
+                    }
+                }
+                // real logic starts here
+                int pipe_cnt = 0;
+                for (int i = 0; i < cmd_cnt; i++)
+                {
+                    if (strcmp(cmd[i], "|") == 0)
+                        pipe_cnt++;
+                }
+                int fd[5][2]; // assume no more than 5 commands
+                for (int i = 0; i < pipe_cnt + 1; i++)
+                {
+                    if (pipe(fd[i]) == -1)
+                    {
+                        printf("3230shell: pipe error\n");
+                        exit(0);
+                    }
+                }
+                for (int i = 0; i < pipe_cnt + 1; i++)
+                {
+                    if (fork() == 0)
+                    {
+                        printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+                        exit(0);
+                    }
+                }
+                for (int i = 0; i < pipe_cnt + 1; i++)
+                {
+                    wait(NULL);
+                }
+                exit(0); // safety net
+            }
+
+            printf("NORMAL COMMAND\n");
+            if (execvp(cmd[0], cmd) == -1)
+                printf("3230shell: '%s': %s\n", cmd[0], strerror(errno));
+            exit(0);
+        }
+        else if (pid > 0)
+        {
+            shell_pid = getpid();
+            kill(pid, SIGUSR1);
+            int status;
+            int dead_child = waitpid(pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                int exit_code = WEXITSTATUS(status);
+                if (exit_code == 100)
+                {
+                    printf("Bye bye!\n");
+                    return 0;
+                }
+            }
+            else if (WIFSIGNALED(status))
+                printf("%s\n", strsignal(status));
         }
     }
     return 0;
