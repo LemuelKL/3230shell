43a63e95bdb96dacb893b76779d4f113abc43c5a
LemuelKL <lemuellee.kl@gmail.com>
2022-10-21 01:52:07 +0800

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -1,5 +1,3 @@
-
-
 #include <sys/resource.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -9,61 +7,34 @@
 #include <errno.h>
 #include <string.h>
 
+void close_fds(int fd[][2], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        close(fd[i][0]);
+        close(fd[i][1]);
+    }
+}
+
 void pipe_expr(int fd[][2], int i, char **args, int pipe_cnt, int expr_cnt, int *expr_idx)
 {
     if (i == 0) // first expr
     {
-        close(fd[i][0]); // first expr does not read
-
-        // close all other fds[j], j != 0
-        for (int j = 1; j < pipe_cnt; j++)
-        {
-            close(fd[j][0]);
-            close(fd[j][1]);
-        }
-
-        // redirect stdout to fd[0][1]
-        // data entrance of entire pipe line
         dup2(fd[i][1], STDOUT_FILENO);
-        close(fd[i][1]);
-
-        execvp(args[expr_idx[i]], args + expr_idx[i]);
     }
     else if (i == expr_cnt - 1) // last expr
     {
-        for (int j = 0; j < pipe_cnt; j++)
-        {
-            close(fd[j][1]);
-            if (j != i - 1)
-                close(fd[j][0]);
-        }
-
-        // only read from fd[i-1][0]
         dup2(fd[i - 1][0], STDIN_FILENO);
-        close(fd[i - 1][0]);
-
-        execvp(args[expr_idx[i]], args + expr_idx[i]);
     }
     else // middle exprs
     {
-        for (int j = 0; j < pipe_cnt; j++)
-        {
-            if (j != i - 1)
-                close(fd[j][0]);
-            if (j != i)
-                close(fd[j][1]);
-        }
-
-        // only read from fd[i-1][0]
         dup2(fd[i - 1][0], STDIN_FILENO);
-        close(fd[i - 1][0]);
-
-        // only write to fd[i][1]
         dup2(fd[i][1], STDOUT_FILENO);
-        close(fd[i][1]);
-
-        execvp(args[expr_idx[i]], args + expr_idx[i]);
     }
+    close_fds(fd, pipe_cnt);
+    if (execvp(args[expr_idx[i]], args + expr_idx[i]) == -1)
+        printf("3230shell: '%s': %s\n", args[expr_idx[i]], strerror(errno));
+    exit(0); // just return to main shell loop
 }
 
 void run_pipe(char **args, int tk_cnt, bool timex_md)
@@ -71,7 +42,7 @@ void run_pipe(char **args, int tk_cnt, bool timex_md)
     // EXPR_1 | EXPR_2 | ... | EXPR_N
     int pipe_cnt = 0;
     int expr_idx[tk_cnt]; // array of pointers to the start of each expression
-    expr_idx[0] = 0; // first expression starts at args[0]
+    expr_idx[0] = 0;      // first expression starts at args[0]
     for (int i = 0; i < tk_cnt; i++)
     {
         if (strcmp(args[i], "|") == 0)
@@ -83,7 +54,7 @@ void run_pipe(char **args, int tk_cnt, bool timex_md)
     }
     int expr_cnt = pipe_cnt + 1;
     int fd[expr_cnt][2];
-    for (int i = 0; i < expr_cnt; i++)
+    for (int i = 0; i < pipe_cnt; i++)
     {
         if (pipe(fd[i]) == -1)
         {
@@ -95,24 +66,19 @@ void run_pipe(char **args, int tk_cnt, bool timex_md)
     for (int i = 0; i < expr_cnt; i++)
     {
         int pid = fork();
+        if (pid == -1)
+        {
+            printf("3230shell: pipe error\n");
+            exit(0);
+        }
         if (pid == 0)
         {
             pipe_expr(fd, i, args, pipe_cnt, expr_cnt, expr_idx);
         }
-        else
-        {
-            child_pids[i] = pid;
-        }
+        child_pids[i] = pid;
     }
-    // leftmost of the pipes is none
-    // WRITE_END of 1st pipe is written by 1st child
-    // rightmost of the pipes is STDOUT
     // parents have no role to play, close all
-    for (int i = 0; i < expr_cnt; i++)
-    {
-        close(fd[i][0]);
-        close(fd[i][1]);
-    }
+    close_fds(fd, pipe_cnt);
     for (int i = 0; i < expr_cnt; i++)
     {
         int status;
